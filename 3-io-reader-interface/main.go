dc71ed098092af9be842f7bd6aaf0b672e2b73e5
acanete <alba.canete@bsc.es>
2024-08-23 16:09:55 +0200

    add io.Reader interface example

--- 3-io-reader-interface/main.go
+++ 3-io-reader-interface/main.go
@@ -7,18 +7,27 @@ import (
 )
 
 type MySlowReader struct {
-	Contents string
+	// variables can be lowercase because we don't want to export them
+	contents string
+	pos      int
 }
 
 // MySlowReader needs a Read function in other to be a Reader and be able to pass it to io.ReadAll
-func (m MySlowReader) Read(p []byte) (n int, err error) {
+// need to pass var by reference (pointer) because if not, m.pos++ takes no effect
+func (m *MySlowReader) Read(p []byte) (n int, err error) {
+	if m.pos+1 <= len(m.contents) { // Pos starts at 0
+		elems := copy(p, m.contents[m.pos:m.pos+1])
+		m.pos++
+		return elems, nil
+	}
 	return 0, io.EOF
 }
 
 func main() {
 
-	mySlowReaderInstance := MySlowReader{
-		Contents: "hello world!",
+	mySlowReaderInstance := &MySlowReader{
+		contents: "hello world!",
+		pos:      0,
 	}
 
 	out, err := io.ReadAll(mySlowReaderInstance)
